0fa914bc5a8bed74f378627be46422d0363ce45f
Janilson Florencio <janilson390@gmail.com>
2025-03-31 11:50:12 -0300

    Aula Camada Core Part 2 - Criando um projeto na linha de comando

--- DevFreela/DevFreela.Core/obj/Debug/net8.0/DevFreela.Core.AssemblyInfo.cs
+++ DevFreela/DevFreela.Core/obj/Debug/net8.0/DevFreela.Core.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("DevFreela.API")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("DevFreela.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0d8948f3b19e3b0075c88b007c4215927ae14b")]
-[assembly: System.Reflection.AssemblyProductAttribute("DevFreela.API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("DevFreela.API")]
+[assembly: System.Reflection.AssemblyProductAttribute("DevFreela.Core")]
+[assembly: System.Reflection.AssemblyTitleAttribute("DevFreela.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
